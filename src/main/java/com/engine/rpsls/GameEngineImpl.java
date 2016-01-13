d312a9de8227d4e24d8fb75571077c9aa08bad0c
Antti Slunga <antti_slunga@hotmail.com>
2016-01-13 23:59:07 +0200

    Injections

--- src/main/java/com/engine/rpsls/GameEngineImpl.java
+++ src/main/java/com/engine/rpsls/GameEngineImpl.java
@@ -3,10 +3,11 @@ package com.engine.rpsls;
 import com.api.rpsls.FightResult;
 import com.api.rpsls.GameEngine;
 import com.api.rpsls.Warrior;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GameEngineImpl implements GameEngine {
 
-	@Override
 	public FightResult fight(Warrior warrior1, Warrior warrior2) {
 		return warrior1.fight(warrior2);
 	}
